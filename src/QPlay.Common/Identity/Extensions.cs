03c23c0f5896b36ba6fe50740ddac129b4f6ce89
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-07-05 23:52:46 +0530

    added identity extensions and very minor refactoring

---
+++ src/QPlay.Common/Identity/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QPlay.Common.Identity;
+
+public static class Extensions
+{
+    public static AuthenticationBuilder ConfigureAuthentication(this IServiceCollection services)
+    {
+        return services.ConfigureOptions<ConfigureJwtBearerOptions>()
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+    }
+}
